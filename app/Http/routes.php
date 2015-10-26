2663192bb260b0221e634296c7b02b676b37a27e
Paolo Davila <paolodavila@iesebre.com>
2015-10-26 17:08:46 +0100

    Authenticated working

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::get('/resource', function () {
 
     $authenticated = false;
-    //Session::set('authenticated',true);
+    Session::set('authenticated',true);
     //dd(Session::all());
 
     \Debugbar::info("Xivato!");
